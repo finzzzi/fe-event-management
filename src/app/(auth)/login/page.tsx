ef180dad75b5bea7eff03ab9f878e838ecfe737e
Syarif Miftahudin <miftahudin.syarif@gmail.com>
2025-05-28 00:27:36 +0700

    added register and login page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,5 +1,11 @@
-const Login = () => {
-  return <div>Login</div>;
-};
+import { LoginForm } from "@/components/login-form";
 
-export default Login;
+export default function LoginPage() {
+  return (
+    <div className="bg-[#F7FAFC] flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm md:max-w-3xl">
+        <LoginForm />
+      </div>
+    </div>
+  );
+}
--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import { RegistrationForm } from "@/components/registration-form";
 
-const Register = () => {
-  return <div>Register</div>;
-};
-
-export default Register;
+export default function RegisterPage() {
+  return (
+    <div className="bg-[#F7FAFC] flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm md:max-w-3xl">
+        <RegistrationForm />
+      </div>
+    </div>
+  );
+}
