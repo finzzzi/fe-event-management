b6bb5681d81ee2be1a5bd8542d099533ce47a42a
Syarif Miftahudin <miftahudin.syarif@gmail.com>
2025-05-28 20:40:21 +0700

    feat: added navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,7 @@
 import Link from "next/link";
 
 const Home = () => {
-  return (
-    <div>
-      <h1 className="text-3xl font-bold">EVENTIO</h1>
-      <div className="flex flex-col">
-        <Link href="/register">Register</Link>
-        <Link href="/login">Login</Link>
-      </div>
-    </div>
-  );
+  return <div>Home</div>;
 };
 
 export default Home;
