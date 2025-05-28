305a7cdbbed335756ea9603ef07f1bfcb1726d54
Syarif Miftahudin <miftahudin.syarif@gmail.com>
2025-05-28 16:39:45 +0700

    feat: add login and register page ui
    * init feat
    
    * added register and login page
    
    * fix images.domains configuration deprecated
    
    * feat: add fullname input
    
    * refactor: edit htmlFor name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import Link from "next/link";
+
 const Home = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold">Home</h1>
+      <h1 className="text-3xl font-bold">EVENTIO</h1>
+      <div className="flex flex-col">
+        <Link href="/register">Register</Link>
+        <Link href="/login">Login</Link>
+      </div>
     </div>
   );
 };
---
+++ src/components/login-form.tsx
@@ -0,0 +1,77 @@
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Image from "next/image";
+
+export function LoginForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card className="overflow-hidden p-0">
+        <CardContent className="grid p-0 md:grid-cols-2">
+          <form className="p-6 md:p-8">
+            <div className="flex flex-col gap-6">
+              <div className="flex flex-col items-center text-center">
+                <h1 className="text-2xl font-bold">Welcome back!</h1>
+                <p className="text-muted-foreground text-balance">
+                  Login to your EVENTIO account
+                </p>
+              </div>
+
+              <div className="grid gap-3">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="email@example.com"
+                  required
+                />
+              </div>
+
+              <div className="grid gap-3">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                  <a
+                    href="#"
+                    className="ml-auto text-sm underline-offset-2 hover:underline"
+                  >
+                    Forgot your password?
+                  </a>
+                </div>
+                <Input
+                  id="password"
+                  type="password"
+                  required
+                  placeholder="password"
+                />
+              </div>
+
+              <Button variant="blue" type="submit" className="w-full">
+                Login
+              </Button>
+
+              <div className="text-center text-sm">
+                Don&apos;t have an account?{" "}
+                <a href="/register" className="underline underline-offset-4">
+                  Sign up
+                </a>
+              </div>
+            </div>
+          </form>
+          <div className="bg-muted relative hidden md:block">
+            <Image
+              src="https://images.unsplash.com/photo-1509824227185-9c5a01ceba0d?q=80&w=2030&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt="Image"
+              fill
+              className="object-cover dark:brightness-[0.2] dark:grayscale"
+            />
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
